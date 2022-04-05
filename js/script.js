bfb5210514cdceab9925a11430da78dc3301ea80
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-04-05 16:31:10 +0000

    didn't add code

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 /**
  * This function calculates area of a circle.
  */
-function calculate () {
+function calculate() {
   // input
   const radius = parseInt(document.getElementById('radius').value)
 
   // process
-  
-  const volume = 1.333 * Math.PI * radius * radius * radius * radius
+
+  const volume = 1.333 * Math.PI * radius * radius * radius
 
   // output
-  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm²'
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
 }
